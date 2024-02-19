d26a153d36d80e326d5b09d0c6bb7ff56d9a2dcd
niccolocaselli <caselli.niccolo.developer@gmail.com>
2024-02-19 23:45:55 +0100

    plot tree docs

--- src/plot_tree.py
+++ src/plot_tree.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 
 def plot_tree(tree):
+    """
+    Disegna un Albero Binario di Ricerca.
+
+    Parameters:
+    - tree (BST): L'albero da disegnare.
+    """
+
     fig, ax = plt.subplots()
     _plot_tree(ax, tree.root, x=0, y=0, level=1)
-    ax.axis('off')
+    ax.axis('off') # Nasconde gli assi
     plt.show()
 
+
+# Funzione ricorsiva per disegnare i nodi dell'albero
 def _plot_tree(ax, node, x, y, level):
     if node is not None:
+        # Disegna il nodo
         ax.annotate(node.key, (x, y), xytext=(x, y),
                 color="white",    
                 ha='center',
@@ -15,11 +25,11 @@ def _plot_tree(ax, node, x, y, level):
                 bbox=dict(boxstyle='circle', fc='#505050')
         )
 
-    xfactor = 1/2
-    y_new = level * -2
+    xfactor = 1/2 # Fattore di scala per la coordinata x
+    y_new = level * -2 # Calcola la nuova coordinata y in base al livello
 
     if node.left is not None:
-        x_new = x - xfactor ** level
+        x_new = x - xfactor ** level # Se è figlio sinistro, si sposta a sinistra il prossimo nodo
         ax.plot([x, x_new], [y, y_new], linewidth=.8,color="#404040")
         _plot_tree(ax, node.left, x_new, y_new, level + 1)
 
@@ -30,10 +40,16 @@ def _plot_tree(ax, node, x, y, level):
 
 
 
-
-
-
 def plot_list_tree(tree):
+
+    """
+    Disegna un Albero Binario di Ricerca con Lista concatenata per chiavi duplicate.
+    Tra parentesi viene indicato il numero di duplicati per ogni chiave.
+
+    Parameters:
+    - tree (BST): L'albero da disegnare.
+    """
+
     fig, ax = plt.subplots()
     _plot_list_tree(ax, tree.root, x=0, y=0, level=1)
     ax.axis('off')
